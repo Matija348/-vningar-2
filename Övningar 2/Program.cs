7c4885f08811bad4809542c8644d302937dbf0cd
Matija Zezelj <matija.zezelj@elev.ga.ntig.se>
2024-11-15 10:09:08 +0100

    Del 2

--- Övningar 2/Program.cs
+++ Övningar 2/Program.cs
@@ -1,7 +1,16 @@
-﻿List<string> leksaker = [
-    "Hide and Seek",
-    "Tag",
-    "Videogames",
-    "Boardgames",
-    "Toys"
+﻿ List<string> leksaker = [
+  "Hide and Seek",
+  "Tag",
+  "Videogames",
+  "Boardgames",    
+  "Toys",
+  "Cards",
 ];
+
+for(int i = 0; i < leksaker.Count; i++)
+{
+    Console.WriteLine($"[{i}]{leksaker[i]}");
+}
+
+
+Console.ReadLine();
